5da49f5d18a4106cf4c8ce02fe1a7a382e8aab41
Ssanchez2002 <samuel.sanchez.cruz@alumnos.upm.es>
2020-05-01 11:15:29 +0200

    Actualizacion del codigo general
    Añadimos al programa la opcion de "iniciar sesion" en el casino haciendo que si estas registrado en el fichero "usuarios" te pida la contraseña y te deje modificar tu saldo.

--- gran_casino_etsidi.c
+++ gran_casino_etsidi.c
@@ -1,20 +1,83 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
 void espacios(int n);
 int aleatorio(int minimo,int maximo);
-
+typedef struct{
+		char nombre[30];
+		char apellido[30];
+		char contrasena[15];
+		float saldo; //int, long int
+		
+}usuario;
 
 int main ()
 {
-  int opcion;
-  char r;
-  	
- 	printf("Buenos dias!! Bienvenido al Gran Casino ETSIDI\n");
+  int opcion,num,tam,i,aux,salir=0,mod;
+  char r,name[30],pass[30];
+  float auf;
+  	usuario persona[30];
+  	FILE *usu;
+  		
+ 	printf("Bienvenido al Gran Casino ETSIDI\n");
 
   //En un futuro aquí se pedirá al usuario que introduzca sus datos y saldo
+	printf("Comencemos con indicar el numero de participantes y sus datos:\n");
+	printf("-Numero de participantes:");
+	scanf(" %i",&tam);
+	//ignorar: persona = malloc(sizeof(usuario) * tam);
 
+				
+	for(i=0;i<tam;i++)
+	{	salir=0;
+		printf("Jugador n %i \n Has jugado alguna vez ya en el Gran Casino?(Si:1//No:0)\n",i+1);
+		scanf("%i",&opcion);
+		if(opcion==1)
+			{	printf("Escribe tu nombre: ");
+				scanf(" %s",name);
+				printf("vamos a buscarte:");
+				usu=fopen("jugadores.txt","r");
+  					if (usu == NULL)
+					{// Si el resultado es NULL mensaje de error
+					printf("Error al abrir el fichero.\n");
+					return -1;
+						}
+				fseek(usu, 0, SEEK_SET);
+				while(feof(usu)==0)
+				{
+						fscanf(usu,"%s\t%s\t%s\t%f \n",persona[i].nombre,persona[i].apellido,persona[i].contrasena,&persona[i].saldo) ;
+									
+					if(strcmp(name, persona[i].nombre)==0)
+							{salir=1;
+								printf("Escribe tu contraseña:");
+								scanf("%s",pass);
+								if(pass==persona[i].contrasena);
+									{	printf("Has iniciado sesión correctamente \n ¿Quieres modificar tu saldo actual ( %f) [Si:1//No:0]?",persona[i].saldo);
+										scanf("%i",&mod);
+										if(mod==1)
+											{		printf("Introduce tu nuevo saldo:");
+											scanf(" %f",&auf);
+											persona[i].saldo=auf;
+												
+											}
+											
+										
+									}
+							}
+							else
+								{	printf("no se ha enocntrado información tuya en el archivo\n");
+								printf("%s",persona[i].nombre);
+								}
+						}
+				}
+				
+								
+							
+				}
+				fclose(usu);
+    
   
 	do
 	{
@@ -64,12 +127,17 @@ int main ()
 		    	printf(" el Bingo\n");
 		    	printf("Pulse 'c' para continuar \n Pulse 'r' para cambiar de juego\n");
 		   		scanf("%s",&r);
+		   			if(r=='c')
+		   				{	
+						   }
 		     	break;
 		      
 			}
  
 	}
 		while(r=='r');
+	
+	
 return 0;}
 	
 	void espacios(int n)

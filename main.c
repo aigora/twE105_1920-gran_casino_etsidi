f00272f9c20563be7861b2cc55a61894e642922f
Ssanchez2002 <samuel.sanchez.cruz@alumnos.upm.es>
2020-05-13 11:15:50 +0200

    Desarrollo de funciones y avance.
    Se empiezan a programar las opciones del  jugador en cada ronda y se mejora la funcion cambio haciendo qeu tambien lleve a cabo el conteo de puntos.

--- main.c
+++ main.c
@@ -15,7 +15,7 @@ int aleatorio(int minimo,int maximo);
 char *crearbaraja(int n);
 char *barajar(char *baraja);
 char *juego(char *baraja,int apuesta, int saldo);
-int cambio(char n);
+int cambio(char cartas[]);
 int main()
 {
 srand(time(NULL));
@@ -29,8 +29,8 @@ int n=0;
 	baraja=barajar(baraja);
 	printf("Baraja:\n %s", baraja);
 	printf("\nSe va a jugar con 2 barajas \nBarajando...\nListo!");
-	printf("\n Su saldo es de %i \n La apuesta minima son 5\n",persona[i].saldo);
-	printf("Que comience el juego!\n");
+	printf("\n Su saldo es de %i. La apuesta minima son 5\n",persona[i].saldo);
+	printf("Que comience el juego, ");
 	printf("Cuanto desea apostar?\n");
 		scanf("%i",&apuesta);
 		if(apuesta<5||apuesta>persona[i].saldo)
@@ -51,13 +51,32 @@ return 0;
 	
 	char *juego(char *baraja,int apuesta, int saldo)
 	{
-		int i=0;
-		char carta1=baraja[i];
-		char carta2=baraja[i+1];
-		char carta3=baraja[i+2];
-		char cartaextra, opcion;
-		printf("Crupier: |%c|\nJugador:|%c|\t|%c|\n",carta1,carta2,carta3);
-		printf("Tienes %i puntos y el Crupier tiene %i puntos|\n",(cambio(carta2)+cambio(carta3)),cambio(carta1));
+		int i=0,n=0;
+		char cartajugador[i];
+		char cartacrupier[i];
+		char opcion;
+		cartacrupier[i]=baraja[n];
+		n++;
+		if(cartacrupier[0]=='0')
+		{
+		printf("Crupier:\t|10|");
+		}
+		else
+		printf("Crupier:\t|%c|",cartacrupier[0]);
+		printf("Jugador: ");
+		for(i=0;i<2;i++){
+			cartajugador[i]=baraja[n];
+			if(cartajugador[i]=='0')
+			{
+				printf("|10|\t");
+			n++;
+			}
+			else
+			printf("|%c|\t",cartajugador[i]);
+			n++;
+		}
+		printf("Tienes %i puntos ",cambio(cartajugador));
+		printf("y el Crupier tiene %i puntos|\n",cambio(cartacrupier));
 		printf("Puedes:\nPedir carta:'p'\nDoblar apuesta:'d'\nPlantarte:'f'\n");
 		scanf(" %c",&opcion);
 			while(opcion!='p'&&opcion!='d'&&opcion!='f')
@@ -65,29 +84,69 @@ return 0;
 				printf("No es una opcion valida");
 				scanf("%c",&opcion);
 				}
-		switch(opcion)
+		do
 		{
-		case 'p':
-		case 'd':
-		case 'f':	
-		
-		
-		}	
+		switch(opcion)
+			{
+			case 'p':
+				i=i+1;
+				cartajugador[i]=baraja[i+2];+
+				printf("Jugador: ");
+				for(i=0;i<n;i++){
+				printf("|%c|\t",cartajugador[i]);
+				}
+				n++;
+				opcion='f';
+				break;
+			case 'd':
+				printf("%i,%i\n",saldo,apuesta);
+				if(apuesta+apuesta>saldo)
+					{
+					printf("No tienes suficiente saldo, cambia de opcion\n");
+					scanf(" %c",&opcion);
+					}			
+					apuesta=apuesta*2;
+					printf("%i",apuesta);
+					scanf(" %c",&opcion);
+				break;
+			case 'f':
+				cartacrupier[i]=baraja[i+3];
+				printf("Crupier: ");
+				for(i=0;i<n;i++){
+				printf("|%c|\t",cartacrupier[i]);
+				}
+				n++;
+				printf("Tienes %i puntos y el Crupier tiene %i puntos|\n",cambio(cartajugador),cambio(cartacrupier));
+			
+				opcion='e';
+				break;		
+			}	
+		}
+		while(opcion!='e');
 		printf("\n %s \n",baraja);
 		return baraja;
 	}
-	int cambio(char n)
+	int cambio(char cartas[])
 	{
-	int num = 0;
-	switch(n)	{
+	int i=0;
+	char n=cartas[0];
+	int total=0,num = 0;
+	while(cartas[i]!='\0')
+	{
+		n=cartas[i];
+		
+		switch(n)	
+		{
 		case 'A':
 			num = 1;
+			total=total+num;
 		break;
 		case 'K':
 		case 'Q':
 		case 'J':
 		case '0':
 			num = 10;
+			total=total+num;
 		break;
 		case '1':
 		case '2':
@@ -99,7 +158,11 @@ return 0;
 		case '8':
 		case '9':
 			num = n - '0';
+			total=total+num;
 		break;
+		}
+	
+	i++;
 	}
-	return num;
-	}
+	return total;
+}

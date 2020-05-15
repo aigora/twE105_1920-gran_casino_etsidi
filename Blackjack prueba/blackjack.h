acbe8a514bb4ac5e258eb7ec5dfc492eb2a7af8b
Ssanchez2002 <samuel.sanchez.cruz@alumnos.upm.es>
2020-05-15 10:35:05 +0200

    FINAL BLACKCJACK
    Se finaliza el juego del blackjack estando totalmente operativo, falta depurar apartados de aspecto, se actualiza la libreria blackjack.h con las 5 funciones que componen el juego. En orden su funcion es crear una baraja, barajarla llevar a cabo una ronda del juego, evaluar si has ganado o perdido tu apuesta y darle el valor numerico a las cartas. Faltaria unicamente unirlo al codigo de gran casino etsidi.

--- Blackjack prueba/blackjack.h
+++ Blackjack prueba/blackjack.h
@@ -3,3 +3,6 @@
 
 char *crearbaraja(int n);
 char *barajar(char *baraja);
+int juego(char *baraja,int apuesta, int saldo);
+int resultado(int jugador, int crupier, int saldo, int apuesta);
+int cambio(char cartas[]);
--- Blackjack prueba/main.c
+++ Blackjack prueba/main.c
@@ -12,235 +12,65 @@ typedef struct{
 }usuario;
 
 int aleatorio(int minimo,int maximo);
-char *crearbaraja(int n);
-char *barajar(char *baraja);
-char *juego(char *baraja,int apuesta, int saldo);
-int cambio(char cartas[]);
+void espacios(int n);
 int main()
 {
 srand(time(NULL));
 usuario persona[30];
 int i, apuesta;
 persona[i].saldo=100000;
-char *baraja,*resultado;
-int n=0;
+char *baraja;
+char reglas,ronda;
+int n=0,resultado;
 	baraja = crearbaraja(2);
 	printf("\n %s \n", baraja);
 	baraja=barajar(baraja);
-	printf("Baraja:\n %s", baraja);
 	printf("\nSe va a jugar con 2 barajas \nBarajando...\nListo!");
-	printf("\n Su saldo es de %i. La apuesta minima son 5\n",persona[i].saldo);
+	printf("\nSu saldo es de %i. La apuesta minima son 5\n",persona[i].saldo);
+	printf("Desea una breve explicacion de las reglas?('y' para si),('n' para no)\n");
+		scanf("%c", &reglas);
+		if(reglas=='y')
+		{
+	 		printf("El blackjack es un juego de cartas en el que el jugador juega contra el crupier. El objetivo es quedar lo mas cerca de\n21 puntos sin pasarse.");
+	 		printf("Las figuras valen 10 puntos y el resto de cartas su valor numerico. El As actua como un comodin y\nvale 11 o 1 puntos dependiendo de lo que ");
+	 		printf("favorezca al jugador. Cada ronda comenzara repartiendo dos cartas al jugador y una al crupier. A partir de aqui el jugador podra doblar la ");
+	 		printf("apuesta, pedir carta hasta que desee (siempre que no supere\nlos 21 puntos) o plantarse. Al plantarse se repartiran cartas al crupier, el cual robara");
+	 		printf("carta siempre que tenga 16\no menos y se plantara con 17 o mas puntos\n");
+		}
 	printf("Que comience el juego, ");
 	printf("Cuanto desea apostar?\n");
 		scanf("%i",&apuesta);
 		if(apuesta<5||apuesta>persona[i].saldo)
 		{
-			printf("Debe cambiar su apuesta");
+			printf("Debe cambiar su apuesta\n");
 			scanf("%i",&apuesta);
 		}
-	resultado= juego(baraja, apuesta, persona[i].saldo);
+	persona[i].saldo= juego(baraja, apuesta, persona[i].saldo);
+	printf("\nTe queda %i saldo, quieres jugar otra ronda?('y' para si),('n' para no)\n",persona[i].saldo);
+	scanf(" %c",&ronda);
+		while(ronda!='n')
+		{
+			if(ronda=='y')
+			{
+			espacios(23);
+			baraja=barajar(baraja);
+			persona[i].saldo= juego(baraja, apuesta, persona[i].saldo);
+			printf("\nTe queda %i saldo, quieres jugar otra ronda?('y' para si),('n' para no)\n",persona[i].saldo);
+			scanf(" %c",&ronda);
+			}
+		}
+	printf("GRACIAS POR JUGAR VUELVA PRONTO");
 	free(baraja);
-	free(resultado);
-return 0;
+	return 0;
 }
 	int aleatorio(int minimo,int maximo)
 	{     int numero;
 		numero=rand()%((maximo-minimo)+1)+minimo;
 		return numero;
 	}
-	
-	char *juego(char *baraja,int apuesta, int saldo)
-	{
-		int i=0,n=0, j=0, k=0;
-		char cartajugador[5];
-		char cartacrupier[5];
-		char opcion;
-		cartacrupier[i]=baraja[n];
-		n++;
-		k++;
-		if(cartacrupier[0]=='0')
-		{
-		printf("Crupier:\t|10|");
+	void espacios(int n)
+	{	int i;
+		for(i=0;i<n;i++)
+		{	printf("\n");
 		}
-		else
-		printf("Crupier:\t|%c|",cartacrupier[0]);
-		printf("Jugador: ");
-		for(i=0;i<2;i++)
-			{
-			cartajugador[i]=baraja[n];
-				if(cartajugador[i]=='0')
-					{
-					printf("|10|\t");
-					n++;
-					j++;
-					}
-				else
-					{
-					printf("|%c|\t",cartajugador[i]);
-					n++;
-					j++;
-					}
-			}
-		printf("Tienes %i puntos ",cambio(cartajugador));
-		printf("y el Crupier tiene %i puntos|\n",cambio(cartacrupier));
-		printf("Puedes:\nPedir carta:'p'\nDoblar apuesta:'d'\nPlantarte:'f'\n");
-		scanf(" %c",&opcion);
-			while(opcion!='p'&&opcion!='d'&&opcion!='f')
-				{
-				printf("No es una opcion valida");
-				scanf("%c",&opcion);
-				}
-				
-		do
-		{
-		switch(opcion)
-			{
-			case 'p':
-				j++;
-				cartajugador[i]=baraja[n];
-				n++;
-				printf("Jugador: ");
-				for(i=0;i<j;i++)
-				{
-				if(cartajugador[i]=='0')
-					{
-					printf("|10|\t");
-					}
-				else
-					{
-					printf("|%c|\t",cartajugador[i]);
-					}
-				}
-				if(cambio(cartajugador)>21)
-				{
-				opcion='e';
-				break;
-				}
-				printf("Tienes %i puntos ",cambio(cartajugador));
-				printf("Quieres pedir otra? (p para pedir o f para terminar la ronda)\n");
-				scanf(" %c",&opcion);
-				break;
-			case 'd':
-				printf("%i,%i\n",saldo,apuesta);
-				if(apuesta+apuesta>saldo)
-					{
-					printf("No tienes suficiente saldo, cambia de opcion\n");
-					scanf(" %c",&opcion);
-					}			
-					apuesta=apuesta*2;
-					printf("Esta es tu nueva apuesta: %i\nQue desea hacer a continuacion?",apuesta);
-					scanf(" %c",&opcion);
-				break;
-			case 'f':
-				if(cambio(cartajugador)>21)
-				{
-				opcion='e';
-				break;
-				}
-				printf("Crupier:");
-				do
-				{
-					i=k;
-					cartacrupier[i]=baraja[n];
-					n++;
-					k++;
-				if(cambio(cartacrupier)>21)
-					{
-						opcion='e';
-						break;
-					}
-				}
-				
-				while(cambio(cartacrupier)<=16);
-				for(i=0;i<k;i++)
-					{
-						if(cartacrupier[i]=='0')
-							{
-								printf("\t|10|");
-							}
-						else
-							printf("\t|%c|",cartacrupier[i]);
-					}
-				
-				printf("\nTienes %i puntos y el Crupier tiene %i puntos|\n",cambio(cartajugador),cambio(cartacrupier));
-			
-				opcion='e';
-				break;		
-			}	
-		}
-		while(opcion!='e');
-		if(cambio(cartajugador)>21)
-					{
-					printf("Tienes %i puntos, son mas de 21 has perdido",cambio(cartajugador));
-					}
-		if(cambio(cartacrupier)>21)
-					{
-					printf("El crupier tiene %i puntos, son mas de 21 has ganado",cambio(cartacrupier));	
-					}
-		if(cambio(cartacrupier)>cambio(cartajugador&&cambio(cartacrupier)<=21))
-					{
-					printf("El crupier tiene mas puntos que tu has perdido J=%i;C=%i",cambio(cartajugador),cambio(cartacrupier));
-					}
-		if(cambio(cartacrupier)<cambio(cartajugador)&&cambio(cartajugador)<=21)
-					{
-					printf("Tienes mas puntos que el Crupier, HAS GANADO! J=%i;C=%i",cambio(cartajugador),cambio(cartacrupier));
-					}
-		if(cambio(cartacrupier)==cambio(cartajugador))
-					{
-					printf("Habeis empatado J=%i;C=%i",cambio(cartajugador),cambio(cartacrupier));
-					}		
-		return baraja;
 	}
-	int cambio(char cartas[])
-	{
-	int i=0,j=0;
-	char n=cartas[0];
-	int total=0,num = 0;
-	while(cartas[i]!='\0')
-	{
-		n=cartas[i];
-		
-		switch(n)	
-		{
-		case 'A':
-			if(total+11<=21)
-			{
-			total=total+11;
-			}
-			else
-			total=total+1;
-		break;
-		case 'K':
-		case 'Q':
-		case 'J':
-		case '0':
-			num = 10;
-			total=total+num;
-		break;
-		case '1':
-		case '2':
-		case '3':
-		case '4':
-		case '5':
-		case '6':
-		case '7':
-		case '8':
-		case '9':
-			num = n - '0';
-			total=total+num;
-		break;
-		}
-	
-	i++;
-	j=i;
-	}
-	if(total>21)
-		{
-			for(i=0;i<j;i++)
-				{
-					if(cartas[i]=='A')
-					total=total-10; 	
-				}			
-		}
-	return total;
-}
